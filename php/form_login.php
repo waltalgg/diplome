820a6ac7281206ef4d8bbe8bfdd34b4a03d7f511
waltal <waltalgg@yandex.ru>
2023-05-29 10:49:42 +0300

    v1.1 php to5 po4

--- php/form_login.php
+++ php/form_login.php
@@ -1,13 +1,18 @@
+<?php
+session_start();
+require_once 'connect.php';
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <link rel="stylesheet" href="../css/style_registration.css">
- <title>Регистрация</title>
+ <title>Вход</title>
 
 </head>
 <body>
   <div class="form_design">
- <form action="log_in.php" method="post">
+ <form action="sign_in.php" method="post">
   <h1>Вход</h1>
   <div class="form-group">
    <input type="email" id="email" name="email" placeholder=" " required>
@@ -17,6 +22,12 @@
    <input type="password" id="password" name="password" placeholder=" " required>
    <label for="password">Пароль</label>
  </div>
+ <div class="login">
+   <p style="color: red;"><b><i> <?
+   echo $_SESSION['error_mes'];
+   unset($_SESSION['error_mes']);
+                                 ?></i></b></p>
+</div>
  <div class="form-group">
    <input type="submit" value="Войти">
  </div>
