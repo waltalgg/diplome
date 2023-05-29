637a313913c6a20589b3f5b0317119803624e39a
waltal <waltalgg@yandex.ru>
2023-05-29 10:49:42 +0300

    v1.1 php to5 (edit) po4 (edit)

--- php/form_registration.php
+++ php/form_registration.php
@@ -1,13 +1,16 @@
+<?php
+session_start();
+require_once 'connect.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <link rel="stylesheet" href="../css/style_registration.css">
  <title>Регистрация</title>
-
 </head>
 <body>
   <div class="form_design">
- <form action="reg_in.php" method="post">
+ <form action="sign_up.php" method="POST">
   <h1>Регистрация</h1>
   <div class="form-group">
    <input type="text" id="username" name="username" placeholder=" " required>
@@ -22,6 +25,16 @@
    <label for="password">Пароль</label>
  </div>
  <div class="form-group">
+  <input type="password" id="password_c" name="password_c" placeholder=" " required>
+  <label for="password">Подтверджение пароля</label>
+</div>
+<div class="login">
+  <p style="color: red;"><b><i> <?php
+    echo $_SESSION['error_mes'];
+    unset ($_SESSION['error_mes']);
+                                ?></i></b></p>
+</div>
+ <div class="form-group">
    <input type="submit" value="Зарегистрироваться">
  </div>
     <div class="login">
@@ -30,6 +43,7 @@
   <div class="login">
      <a href="index.php">Назад на главную</a>
  </div>
+
 </div>
  </form>
 </body>
